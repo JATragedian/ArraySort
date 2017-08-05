2167a3477b983c05923e53557d15b81f1b1dc085
Alexander Isaev <JATragedian@yandex.ru>
2017-08-05 23:31:16 +0300

    StableIndex

--- newArraySort/newArraySort/SortThis.h
+++ newArraySort/newArraySort/SortThis.h
@@ -5,31 +5,19 @@ template <class T> void sortThis(T *array, unsigned first, unsigned last)
 {
 	unsigned left = first;
 	unsigned right = last;
-	T *mid = &array[(left + right) / 2];
-	T *ptrLeft = &array[left];
-	T *ptrRight = &array[right];
+	T mid = array[(left + right) / 2];
 
 	while (left <= right) {
-		while (ptrLeft < mid) {
-			ptrLeft++;
+		while (array[left] < mid) {
 			left++;
 		}
 
-		while (ptrRight > mid) {
-			ptrRight--;
+		while (array[right] > mid) {
 			right--;
 		}
 
 		if (left <= right) {
-
-			T temp = *ptrLeft;
-			ptrLeft = ptrRight;
-			ptrRight = &temp;
-
-			ptrLeft++;
-			ptrRight--;
-			left++;
-			right--;
+			std::swap(array[left++], array[right--]);
 		}
 	}
 
