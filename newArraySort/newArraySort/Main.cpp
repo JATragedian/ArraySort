c25b22621b33b098af701b66c3890dc244c7ebc2
Alexander Isaev <JATragedian@yandex.ru>
2017-08-05 23:13:55 +0300

    Pointers(Not working)

--- newArraySort/newArraySort/Main.cpp
+++ newArraySort/newArraySort/Main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
+﻿#include <iostream>
+#include <ctime>
 #include "SortThis.h"
+
+#define SIZE 3854
 
 int main()
 {
-	unsigned long array[3854];
+	int array[SIZE];
+
+	for (unsigned i = 0; i < SIZE; i++) {
+		array[i] = rand() % 91 + 65;
+	}
+
+	clock_t start = clock();
+
+	sortThis(array, 0, SIZE - 1);
+
+	clock_t end = clock();
+
+	for (unsigned i = 0; i < SIZE; i++) {
+		std::cout << array[i] << ' ';
+	}
+
+	std::cout << "\n\n" << "Speed of sorting execution: " << (double) (end - start) / SIZE << '\n';
+
+	system("pause >> NULL");
 
 	return 0;
 }
--- newArraySort/newArraySort/SortThis.h
+++ newArraySort/newArraySort/SortThis.h
@@ -1,9 +1,45 @@
-#ifndef SORTTHIS_H
+﻿#ifndef SORTTHIS_H
 #define SORTTHIS_H
 
-template <class T> void SortThis(T *array)
+template <class T> void sortThis(T *array, unsigned first, unsigned last)
 {
+	unsigned left = first;
+	unsigned right = last;
+	T *mid = &array[(left + right) / 2];
+	T *ptrLeft = &array[left];
+	T *ptrRight = &array[right];
 
+	while (left <= right) {
+		while (ptrLeft < mid) {
+			ptrLeft++;
+			left++;
+		}
+
+		while (ptrRight > mid) {
+			ptrRight--;
+			right--;
+		}
+
+		if (left <= right) {
+
+			T temp = *ptrLeft;
+			ptrLeft = ptrRight;
+			ptrRight = &temp;
+
+			ptrLeft++;
+			ptrRight--;
+			left++;
+			right--;
+		}
+	}
+
+	if (first < right) {
+		sortThis(array, first, right);
+	}
+
+	if (last > left) {
+		sortThis(array, left, last);
+	}
 }
 
 #endif
