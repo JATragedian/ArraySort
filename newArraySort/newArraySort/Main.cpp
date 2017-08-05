9ccf2ee69b63445d3fb92e81afc445df4399d8c3
Alexander Isaev <JATragedian@yandex.ru>
2017-08-05 19:19:26 +0300

    Commit

---
+++ newArraySort/newArraySort/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SortThis.h"
+
+int main()
+{
+	unsigned long array[3854];
+
+	return 0;
+}
---
+++ newArraySort/newArraySort/SortThis.h
@@ -0,0 +1,9 @@
+#ifndef SORTTHIS_H
+#define SORTTHIS_H
+
+template <class T> void SortThis(T *array)
+{
+
+}
+
+#endif
