8617ed9ed69ee7ca9e2044e0d415fcf588d63bf4
Alexander Isaev <JATragedian@yandex.ru>
2017-08-06 20:35:09 +0300

    StableButNotPointers

--- newArraySort/newArraySort/SortThis.h
+++ newArraySort/newArraySort/SortThis.h
@@ -1,61 +1,23 @@
 ﻿#ifndef SORTTHIS_H
 #define SORTTHIS_H
 
-//template <class T> void sortThis(T *array, unsigned first, unsigned last)
-//{
-//	unsigned left = first;
-//	unsigned right = last;
-//	T *mid = &array[(left + right) / 2];
-//
-//	while (left <= right) {
-//		while (array[left] < mid) {
-//			left++;
-//		}
-//
-//		while (array[right] > mid) {
-//			right--;
-//		}
-//
-//		if (left <= right) {
-//			std::swap(array[left++], array[right--]);
-//		}
-//	}
-//
-//	if (first < right) {
-//		sortThis(array, first, right);
-//	}
-//
-//	if (last > left) {
-//		sortThis(array, left, last);
-//	}
-//}
-
 template <class T> void sortThis(T *array, unsigned first, unsigned last)
 {
 	unsigned left = first;
 	unsigned right = last;
 	T *mid = &array[(left + right) / 2];
-	T *ptrLeft = &array[left];
-	T *ptrRight = &array[right];
 
 	while (left <= right) {
-		while (ptrLeft < mid) {
+		while (array[left] < *mid) {
 			left++;
-			ptrLeft++;
 		}
 
-		while (ptrRight > mid) {
+		while (array[right] > *mid) {
 			right--;
-			ptrRight--;
 		}
 
 		if (left <= right) {
-			T temp = *ptrLeft;
-			ptrLeft = ptrRight;
-			ptrRight = &temp;
-
-			ptrLeft++;
-			ptrRight--;
+			std::swap(array[left++], array[right--]);
 		}
 	}
 
