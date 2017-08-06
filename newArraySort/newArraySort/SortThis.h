e0602cf5a4236c64845143b8c4992d1816c23e39
Alexander Isaev <JATragedian@yandex.ru>
2017-08-06 20:31:40 +0300

    StillNotWorking

--- newArraySort/newArraySort/SortThis.h
+++ newArraySort/newArraySort/SortThis.h
@@ -1,23 +1,61 @@
 ﻿#ifndef SORTTHIS_H
 #define SORTTHIS_H
 
+//template <class T> void sortThis(T *array, unsigned first, unsigned last)
+//{
+//	unsigned left = first;
+//	unsigned right = last;
+//	T *mid = &array[(left + right) / 2];
+//
+//	while (left <= right) {
+//		while (array[left] < mid) {
+//			left++;
+//		}
+//
+//		while (array[right] > mid) {
+//			right--;
+//		}
+//
+//		if (left <= right) {
+//			std::swap(array[left++], array[right--]);
+//		}
+//	}
+//
+//	if (first < right) {
+//		sortThis(array, first, right);
+//	}
+//
+//	if (last > left) {
+//		sortThis(array, left, last);
+//	}
+//}
+
 template <class T> void sortThis(T *array, unsigned first, unsigned last)
 {
 	unsigned left = first;
 	unsigned right = last;
-	T mid = array[(left + right) / 2];
+	T *mid = &array[(left + right) / 2];
+	T *ptrLeft = &array[left];
+	T *ptrRight = &array[right];
 
 	while (left <= right) {
-		while (array[left] < mid) {
+		while (ptrLeft < mid) {
 			left++;
+			ptrLeft++;
 		}
 
-		while (array[right] > mid) {
+		while (ptrRight > mid) {
 			right--;
+			ptrRight--;
 		}
 
 		if (left <= right) {
-			std::swap(array[left++], array[right--]);
+			T temp = *ptrLeft;
+			ptrLeft = ptrRight;
+			ptrRight = &temp;
+
+			ptrLeft++;
+			ptrRight--;
 		}
 	}
 
